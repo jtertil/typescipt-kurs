f7dca725ad6f0fb60394e77e2173fdb654e28d85
jtertil <jtertil@gmail.com>
2019-02-16 13:51:46 +0100

    initial

---
+++ basicTypes/basicTypes.js
@@ -0,0 +1,11 @@
+var BasicType = /** @class */ (function () {
+    function BasicType() {
+        this.name = 'Reksio';
+        this.age = 5;
+        this.colors = ['czarny', 'biały'];
+        this.dates = [new Date(), new Date()];
+        this.pair = ['Sobota', 16];
+        this.xyz = 123;
+    }
+    return BasicType;
+}());
---
+++ basicTypes/basicTypes.ts
@@ -0,0 +1,8 @@
+class BasicType{
+	name: string = 'Reksio';
+	age: number = 5;
+	colors: string[] =['czarny', 'biały'];
+	dates: Array<Date> = [new Date(),new Date()]; 
+	pair: [string, number] = ['Sobota', 16];
+	xyz: any = 123;
+}
